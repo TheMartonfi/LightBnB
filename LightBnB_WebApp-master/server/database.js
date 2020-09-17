07cd1af1bfe857fc2a836cb48e4ef068c8f4ddd7
TheMartonfi <carlmartonfi@hotmail.com>
2020-09-17 20:02:28 +0000

    Make getAllReservations use database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -19,7 +19,7 @@ const pool = new Pool({
  */
 const getUserWithEmail = function(email) {
   return pool.query(`
-  SELECT email, password
+  SELECT id, name, email, password
   FROM users
   WHERE email = $1;
   `, [email])
@@ -40,7 +40,7 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 const getUserWithId = function(id) {
   return pool.query(`
-  SELECT id, email, password
+  SELECT id, name, email, password
   FROM users
   WHERE id = $1;
   `, [id])
@@ -61,10 +61,6 @@ exports.getUserWithId = getUserWithId;
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser =  function(user) {
-  // const userId = Object.keys(users).length + 1;
-  // user.id = userId;
-  // users[userId] = user;
-  // return Promise.resolve(user);
   return pool.query(`
   INSERT INTO users (name, email, password)
   VALUES ($1, $2, $3)
@@ -83,8 +79,25 @@ exports.addUser = addUser;
  * @param {string} guest_id The id of the user.
  * @return {Promise<[{}]>} A promise to the reservations.
  */
+
+// Should not be today's date
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool.query(`
+  SELECT properties.id, properties.title, properties.cost_per_night, reservations.start_date,
+    reservations.end_date, AVG(rating) AS average_rating, thumbnail_photo_url,
+    number_of_bedrooms, number_of_bathrooms, parking_spaces
+  FROM reservations
+  JOIN properties ON reservations.property_id = properties.id
+  JOIN property_reviews ON property_reviews.property_id = properties.id
+  WHERE reservations.guest_id = $1
+  AND reservations.end_date < now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+  .then(res => {
+    return res.rows;
+  });
 }
 exports.getAllReservations = getAllReservations;
 
